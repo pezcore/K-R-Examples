a8865e53f6fed4cc4dca96d8c77e7c23775dc960
Tom Boyd <tvboyd23@gmail.com>
2018-02-17 13:56:55 -0500

    JSF x112

--- exercises/x112.c
+++ exercises/x112.c
@@ -7,15 +7,19 @@
 #define IN 1    /* inside a word */
 #define OUT 0   /* outside a word */
 
-main(){
+main()
+{
     char c, state = 0;
-    while ((c = getchar()) != EOF) {
-        if ((c == '\t' || c == 0x20) && state) {
+    while ((c = getchar()) != EOF)
+    {
+        if ((c == '\t' || c == 0x20) && state)
+        {
             while((c = getchar()) == '\t' || c == 0x20);
             putchar('\n');
             putchar(c);
         }
-        else {
+        else
+        {
             state = 1;
             putchar(c);
         }
