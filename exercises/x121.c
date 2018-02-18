d76a89d72f05054bc0be5eb71b8acee7e2ed51c8
Tom Boyd <tvboyd23@gmail.com>
2018-02-17 19:52:44 -0500

    started x121

--- exercises/x121.c
+++ exercises/x121.c
@@ -5,5 +5,30 @@
  * stop, which should be given preference?
  */
 
-#include<stdio.h>
-#define n 4
+#include <stdio.h>
+
+#define TAB 4 // Number of spaces of one tab.
+
+main()
+{
+    int c, i = 0, j;
+
+    while((c = getchar()) != EOF)
+    {
+        if(c == ' ')
+        {
+            ++i; /* This is a counter of white spaces. */
+            if((i % TAB) == 0)   /* Every group of a number of 'TAB' */
+                putchar('\t');   /* spaces is replaced by a tab. */
+        }
+        else
+        {
+
+        for(j = 0; j < (i % TAB); ++j)  /* Every group smaller than */
+            putchar(' ');               /* 'TAB' spaces is untouched. */
+
+        putchar(c); /* Well, there exist other characters but spaces. */
+        i = 0;
+        }
+    }
+}
